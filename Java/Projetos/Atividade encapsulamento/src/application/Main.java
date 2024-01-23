8c9f8f0734037d9757f87bdb252a9b4d160abcc9
Willian Oliveira <willi12345677@gmail.com>
2024-01-23 09:32:52 -0300

    git add
    321

---
+++ Java/Projetos/Atividade encapsulamento/src/application/Main.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.util.Scanner;
+import entities.Class;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Class banco;
+
+        System.out.println("Digite o numero da conta");
+        int numConta = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Digite o nome da conta");
+        String nomeConta = sc.nextLine();
+
+        System.out.println("Tem Dinheiro \"S\"/\"N\"");
+        char temDinheiro = sc.nextLine().charAt(0);
+
+
+        if (temDinheiro == 'S' || temDinheiro == 's' ){
+            System.out.println("Digite o valor de deposito");
+            double depoistoConta= sc.nextDouble();
+            banco = new Class(numConta,nomeConta,depoistoConta);
+
+
+        }else {
+            banco = new Class(numConta,nomeConta);
+
+
+        }
+
+        System.out.println(banco);
+        System.out.println("Enter a deposit value:");
+        banco.deposito(sc.nextDouble());
+        System.out.println("Updated account data: " + banco);
+        System.out.println("Enter a withdraw value:");
+        banco.retirada(sc.nextDouble());
+        System.out.println("Updated account data: " + banco);
+
+
+
+
+        sc.close();
+    }
+}
---
+++ Java/Projetos/Atividade encapsulamento/src/entities/Class.java
@@ -0,0 +1,57 @@
+
+package entities;
+
+public class Class {
+    private int numero;
+    private String nome;
+    private double valorConta;
+
+    public Class(int numero, String nome, double valorConta) {
+        this.setNumero(numero);
+        this.setNome(nome);
+        this.valorConta = valorConta;
+    }
+
+
+    public Class(int numero, String nome) {
+        this.setNumero(numero);
+        this.setNome(nome);
+
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getValorConta() {
+        return valorConta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+
+
+    public void deposito(double valorConta){
+        this.valorConta += valorConta;
+    }
+    public void retirada(double valorConta){
+        this.valorConta -= valorConta;
+    }
+
+    public String toString() {
+        return "Account: " + numero + ", Holder: " + nome + ", Balance: $" + valorConta;
+    }
+}
+
+
